f489fad9d9db7a46888d15b3e19e3317160c7651
Matheus Santos Rangel <matheusrangelti@gmail.com>
2024-07-26 18:08:21 -0300

    Add files via upload

---
+++ Calculador de Área de Paralelogramo.py
@@ -0,0 +1,15 @@
+#Pseudocódigo: 
+#Exiba "Bem-vindo ao Calculador de Área de Paralelogramo"
+#Peça para o usuário inserir o comprimento da base
+#Armazene o comprimento da base em uma variável
+#Peça para o usuário inserir a altura 
+#Armazene a altura em uma variável
+#Calcule a área do paralelograma: base * altura
+#Armazene o resultado em uma variável 
+#Finalmente, exiba o resultado
+
+print("Bem-vindo ao Calculador de Área de Paralelogramo")
+base = float(input("Insira o comprimento da base: ")) 
+altura = float(input("Insira a altura: "))
+area = base * altura 
+print("A área do paralelogramo é: ", area) 
